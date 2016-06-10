8e1a4ec5037af9434bcd84b4f305ce8d9dc83d7f
Minwoo-Kim <bluesky@hatiolab.com>
2016-06-10 16:28:48 +0900

    .

--- example-code/src/main/java/com/example/code/spring/bean/ClassPathScanningCandidateComponentProviderEx.java
+++ example-code/src/main/java/com/example/code/spring/bean/ClassPathScanningCandidateComponentProviderEx.java
@@ -2,10 +2,12 @@ package com.example.code.spring.bean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,22 @@ public class ClassPathScanningCandidateComponentProviderEx {
 			}
 		}
 		return classList;
+	}
+
+	public List<Class<?>> scanEntity_Two() {
+		// Build pattern to lookup implementation class
+		Pattern pattern = Pattern.compile(".*\\.*" + "Stamp");
+
+		// Build classpath scanner and lookup bean definition
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.addIncludeFilter(new RegexPatternTypeFilter(pattern));
+
+		String basePackage = "xyz.elidom.base.entity.basic";
+		// scan from base package
+		for (BeanDefinition bd : scanner.findCandidateComponents(basePackage)) {
+			String beanName = bd.getBeanClassName();
+			System.out.println(beanName);
+		}
+		return null;
 	}
 }
