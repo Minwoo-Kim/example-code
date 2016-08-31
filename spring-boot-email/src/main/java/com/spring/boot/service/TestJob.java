af2061729932cc17b1eeedb7e65889fae1cff055
Minwoo-Kim <bluesky@hatiolab.com>
2016-08-31 14:55:38 +0900

    ..

--- spring-boot-email/src/main/java/com/spring/boot/service/TestJob.java
+++ spring-boot-email/src/main/java/com/spring/boot/service/TestJob.java
@@ -37,7 +37,7 @@ public class TestJob {
 	@Scheduled(fixedRate = 60000)
 	public void doExecuteJob() {
 		try {
-			String url = "http://www.airbusan.com/web/bookingApi/domesticAvail?bookingCategory=Individual&depDate=20160914&depCity=GMP&arrCity=PUS&foc=N&bookingClass=ES";
+			String url = "https://www.airbusan.com/web/bookingApi/domesticAvail?bookingCategory=Individual&depDate=20160916&depCity=PUS&arrCity=GMP&foc=N&bookingClass=ES";
 
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -51,10 +51,10 @@ public class TestJob {
 			con.setRequestProperty("Accept", "application/json, text/javascript, */*; q=0.01");
 			con.setRequestProperty("X-Requested-With", "XMLHttpRequest");
 			con.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
-			con.setRequestProperty("Referer","http://www.airbusan.com/web/individual/booking/domestic?bookingCategory=Individual&foc=N&domesticTripType=OW&depCity=GMP&arrCity=PUS&dateGoing=2016-09-14&passengerCountCorp=0&passengerCountAdult=1&RR4H5HJOIWT43TOLDXHX=1&passengerCountChild=0&passengerCountInfant=0");
+			con.setRequestProperty("Referer","https://www.airbusan.com/web/individual/booking/domestic?bookingCategory=Individual&foc=N&domesticTripType=OW&depCity=PUS&arrCity=GMP&dateGoing=2016-09-16&passengerCountCorp=0&passengerCountAdult=1&EVLTQECHFLQSSSOK0MCZ=1&passengerCountChild=0&passengerCountInfant=0");
 			con.setRequestProperty("Accept-Encoding", "gzip, deflate, sdch");
 			con.setRequestProperty("Accept-Language", "ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4");
-			con.setRequestProperty("Cookie","_gat=1; _ga=GA1.2.1507180003.1472201517; wcs_bt=bccd2d8446215c:1472202178|s_11681f2fc3c3:1472202178; NetFunnel_ID=5002%3A200%3Akey%3D9D53A49D6D098E9DAE8ACF2B9A2BB08BD097A552E0DD0FE5E0FAB443795DC685B5A3846B6B19D1CEF64E7B5579F594D73042F67BFC14CCE3DD7CEA288DF28A8C26898FFA2CA72DFE6D0B721F86BF375941A98A8F44ABDA306BE0249DA0254E604FC18FFE5E722ACA3C738AA434385B796963417661696C2C302C322C312C30%26nwait%3D0%26nnext%3D0%26tps%3D0%26ttl%3D0%26ip%3Dpromo.airbusan.com%26port%3D80");
+			con.setRequestProperty("Cookie","_gat=1; _ga=GA1.2.1507180003.1472201517; wcs_bt=bccd2d8446215c:1472622327|s_11681f2fc3c3:1472622327; NetFunnel_ID=5002%3A200%3Akey%3D66FD0DF05F3BF186E683C173870F2F68583CDD064C0BD07FEF6390DCBAA30F46AB0E8A8697F175287D17525A589ECD8B19D13C34112F4F6616CA3CE80E7363D626898FFA2CA72DFE6D0B721F86BF375941A98A8F44ABDA306BE0249DA0254E604FC18FFE5E722ACA3C738AA434385B796963417661696C2C302C332C312C30%26nwait%3D0%26nnext%3D0%26tps%3D0%26ttl%3D0%26ip%3Dpromo.airbusan.com%26port%3D80");
 
 			String inputLine;
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -72,6 +72,11 @@ public class TestJob {
 			StringJoiner printContents = new StringJoiner("\n");
 			StringJoiner emailContents = new StringJoiner("<br>");
 			for (Map<String, String> data : listMap) {
+				int depTime = Integer.parseInt(data.get("depTime"));
+				if(1300 < depTime || depTime < 1000) {
+					continue;
+				}
+				
 				int count = Integer.parseInt(data.get("availSeat"));
 				String seat = new StringBuilder().append(data.get("depTime")).append(" : ").append(count).toString();
 				if (count > 0) {
